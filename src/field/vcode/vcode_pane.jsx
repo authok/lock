0a3add4484f0bfdffb1138c14e7eb6f97dabae5f
newtalentxp <newtalentxp@126.com>
2022-01-21 02:42:33 +0800

    fix

--- src/field/vcode/vcode_pane.jsx
+++ src/field/vcode/vcode_pane.jsx
@@ -1,57 +1,63 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import VcodeInput from '../../ui/input/vcode_input';
 import * as l from '../../core/index';
 import * as c from '../index';
 import { swap, updateEntity } from '../../store/index';
 import { setVcode } from '../vcode';
 import { sendSMS } from '../../connection/passwordless/actions';
+import useCountDown from '../../hooks/useCountDown';
 
-export default class VcodePane extends React.Component {
-  handleVcodeChange = e => {
+export default function VcodePane({ lock, placeholder, resendLabel }) {
+  const [targetDate, setTargetDate] = useState();
+
+  const handleVcodeChange = useCallback(e => {
     e.preventDefault();
-    swap(updateEntity, 'lock', l.id(this.props.lock), setVcode, e.target.value);
+    swap(updateEntity, 'lock', l.id(lock), setVcode, e.target.value);
+  }, []);
+
+  const handleSendVCode = e => {
+    e.preventDefault();
+    const send = l.hasSomeConnections(lock, 'passwordless', 'sms') ? sendSMS : null;
+    if (send) {
+      send(l.id(lock));
+
+      setTargetDate(Date.now() + 60000);
+    };
   };
 
-  render() {
-    const { lock, placeholder, resendLabel } = this.props;
+  const [countdown] = useCountDown({
+    targetDate,
+    onEnd: () => {
+    },
+  });
 
-    return (
-      <div className="ant-row ant-row-middle ant-row-space-between">
-        <div className="ant-col ant-col-15">
-          <VcodeInput
-            lockId={l.id(lock)}
-            value={c.vcode(lock)}
-            isValid={!c.isFieldVisiblyInvalid(lock, 'vcode') && !l.globalError(lock)}
-            onChange={this.handleVcodeChange}
-            placeholder={placeholder}
-            disabled={l.submitting(lock)}
-          />
-        </div>
-        <div className="ant-col ant-col-8 ant-col-offset-1">
-          <Resend label={resendLabel} model={lock} />
-        </div>
+  return (
+    <div className="ant-row ant-row-middle ant-row-space-between">
+      <div className="ant-col ant-col-15">
+        <VcodeInput
+          lockId={l.id(lock)}
+          value={c.vcode(lock)}
+          isValid={!c.isFieldVisiblyInvalid(lock, 'vcode') && !l.globalError(lock)}
+          onChange={handleVcodeChange}
+          placeholder={placeholder}
+          disabled={l.submitting(lock)}
+        />
       </div>
-    );
-  }
+      <div className="ant-col ant-col-8 ant-col-offset-1">
+        <button 
+          disabled={countdown !== 0}
+          className="ant-btn authok-lock-resend-code" 
+          onClick={handleSendVCode}>
+           {countdown === 0 ? resendLabel : `${Math.round(countdown / 1000)}秒`}
+        </button>
+      </div>
+    </div>
+  );
 }
 
 VcodePane.propTypes = {
   lock: PropTypes.object.isRequired,
   placeholder: PropTypes.string.isRequired,
   resendLabel: PropTypes.string.isRequired,
-};
-
-
-const Resend = ({ label, model }) => {
-  const handleSendVCode = e => {
-    e.preventDefault();
-    const send = l.hasSomeConnections(model, 'passwordless', 'sms') ? sendSMS : null;
-    if (send) send(l.id(model));
-  };
-
-  return <button 
-    className="ant-btn authok-lock-resend-code"
-    onClick={handleSendVCode}
-  >{label}</button>;
 };
